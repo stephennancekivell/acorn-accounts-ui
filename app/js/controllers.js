ef406228c5fc2b9be95cda4bff9dffb89197f09f
Stephen Nancekivell <stephennancekivell@gmail.com>
2013-05-01 19:56:11 +1200

    make new password

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,8 +7,15 @@
 // }]);
 
 angular.module('myApp.controllers', ['myApp.services']).
-	controller('PasswordListCtrl', ['Passwords', '$scope', function(Passwords, $scope) {
+	controller('PasswordListCtrl', ['Passwords', '$scope', '$location', function(Passwords, $scope, $location) {
 		$scope.passwords = Passwords.query();
+
+		$scope.makeNew = function(){
+			Passwords.save({id:-1, password:''},
+				function ok(data){
+					$location.url('password/'+data.id);
+			});
+		};
 	}])
 	.controller('PasswordCtrl', ['$routeParams', '$scope', 'Passwords', function($routeParams, $scope, Passwords) {
 		$scope.password = Passwords.get({id:$routeParams.id});
