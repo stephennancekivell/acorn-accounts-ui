3a0083a248ab0f7deebffd7499b6c30b15ea2996
Stephen Nancekivell <stephennancekivell@gmail.com>
2013-05-01 22:53:02 +1200

    tabs

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,7 @@
 
 
 // Declare app level module which depends on filters, and services
-var app = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers', 'ui.event', 'ui.if']).
+var app = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers', 'ui.event', 'ui.if', 'ui.bootstrap']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/p/:id', {templateUrl: 'partials/passwordList.html', controller: 'PasswordListCtrl'});
     // $routeProvider.when('/p/:id', {templateUrl: 'partials/password.html', controller: 'PasswordCtrl'});
