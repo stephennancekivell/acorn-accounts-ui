13c810de78d6609fc932e2b96888806ec3415c68
Stephen Nancekivell <stephennancekivell@gmail.com>
2013-05-01 20:54:36 +1200

    save updates

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,7 @@
 
 
 // Declare app level module which depends on filters, and services
-var app = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
+var app = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers', 'ui.event']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/list', {templateUrl: 'partials/passwordList.html', controller: 'PasswordListCtrl'});
     $routeProvider.when('/password/:id', {templateUrl: 'partials/password.html', controller: 'PasswordCtrl'});
--- app/js/services.js
+++ app/js/services.js
@@ -4,5 +4,5 @@ var base_url = 'http://127.0.0.1/play/';
 
 angular.module('myApp.services', ['ngResource']).
 	factory('Passwords', ['$resource', function($resource){
-		return $resource(base_url+'passwords/:id');
+		return $resource(base_url+'passwords/:id', {id:'@id'});
 	}]);
