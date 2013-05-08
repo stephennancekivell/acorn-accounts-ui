d02f0acfe502faecd7350cfc783cbe0f4145f4ac
Stephen Nancekivell <stephennancekivell@gmail.com>
2013-05-08 21:50:47 +1200

    persist the selected id by url

--- app/js/app.js
+++ app/js/app.js
@@ -2,8 +2,12 @@
 
 var app = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'ui.event', 'ui.if', 'ui.bootstrap', 'btford.dragon-drop']).
 	config(['$routeProvider', function($routeProvider) {
-		$routeProvider.when('/p/:id', {templateUrl: 'partials/Password.html', controller: 'PasswordListCtrl'});
+		$routeProvider.when('/p', {
+			templateUrl: 'partials/Password.html',
+			controller: 'PasswordListCtrl',
+			reloadOnSearch: false
+		});
 		$routeProvider.when('/u/:id', {templateUrl: 'partials/User.html', controller: 'UserCtrl'});
 		$routeProvider.when('/g/:id', {templateUrl: 'partials/Group.html', controller: 'GroupCtrl'});
-		$routeProvider.otherwise({redirectTo: '/p/'});
+		$routeProvider.otherwise({redirectTo: '/p'});
 	}]);
