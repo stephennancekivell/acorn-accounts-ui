b7ee07b23eb3a4504a8a6aeb33688eeaf46a40ae
Stephen Nancekivell <stephennancekivell@gmail.com>
2013-06-28 20:55:07 +1200

    Fixed testing

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -3,9 +3,19 @@ basePath = '../';
 files = [
   JASMINE,
   JASMINE_ADAPTER,
-  'app/lib/angular/angular.js',
-  'app/lib/angular/angular-*.js',
-  'test/lib/angular/angular-mocks.js',
+  
+  'app/components/angular/angular.js',
+  'app/components/angular-resource/angular-resource.js',
+  'app/components/angular-cookies/angular-cookies.js',
+  'app/components/angular-mocks/angular-mocks.js',
+  'app/components/angular-ui-utils/modules/event/event.js',
+  'app/components/angular-ui-utils/modules/if/if.js',
+  'app/components/angular-ui-utils/modules/route/route.js',
+  'app/components/underscore/underscore.js',
+  'app/components/angular-dragon-drop/dragon-drop.js',
+  'app/components/angular-http-auth/src/http-auth-interceptor.js',
+  'app/lib/ui-bootstrap-tpls.js',
+
   'app/js/**/*.js',
   'test/unit/**/*.js'
 ];
